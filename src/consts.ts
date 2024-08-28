3e73d3c912165ff4e9b22abb25e0eca6263a3a18
Hopsken <shaowei5@icloud.com>
2024-08-28 22:56:43 +0800

    fix

--- src/consts.ts
+++ src/consts.ts
@@ -1,9 +1,9 @@
 export const siteSettings = {
-  title: "Hopsken' Notes",
-  description: 'A digital garden full of notes and thoughts from Hopsken',
+  title: "Hopsken's Notes",
+  description: 'A digital garden full of notes and thoughts from Hopsken'
 }
 
 export const paneSettings = {
   width: 625,
-  labelWidth: 40,
+  labelWidth: 40
 }
