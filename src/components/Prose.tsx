f54db020e9005cb66737b95dfa6cce9ec25f56df
Hopsken <shaowei5@icloud.com>
2024-08-28 23:06:47 +0800

    fix style

--- src/components/Prose.tsx
+++ src/components/Prose.tsx
@@ -2,12 +2,7 @@ import { PropsWithChildren } from 'react'
 
 export function Prose(props: PropsWithChildren<{}>) {
   return (
-    <article
-      className='prose dark:prose-invert
-  prose-h1:font-bold prose-h1:text-xl
-  prose-a:text-blue-600 prose-p:text-justify prose-img:rounded-xl
-  prose-headings:underline'
-    >
+    <article className='prose dark:prose-invert prose-h1:text-xl prose-h1:font-bold prose-p:text-justify prose-a:text-blue-600 prose-img:rounded-xl'>
       {props.children}
     </article>
   )
