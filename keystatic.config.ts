cff86ef6dbbcf94991e46075e1d4472fb0a97dc4
Hopsken <shaowei5@icloud.com>
2024-08-28 19:23:27 +0800

    init

---
+++ keystatic.config.ts
@@ -0,0 +1,38 @@
+// keystatic.config.ts
+import { config, fields, collection } from '@keystatic/core'
+
+export default config({
+  storage: {
+    kind: 'local',
+  },
+  collections: {
+    posts: collection({
+      label: 'Posts',
+      slugField: 'title',
+      path: 'src/content/posts/*',
+      format: { contentField: 'content' },
+      schema: {
+        title: fields.slug({
+          name: { label: 'Title', validation: { isRequired: true } },
+        }),
+        description: fields.text({
+          label: 'Description',
+          validation: { isRequired: false },
+        }),
+        pubDate: fields.date({
+          label: 'Publish date',
+          defaultValue: { kind: 'today' },
+          validation: {
+            isRequired: true,
+          },
+        }),
+        updatedDate: fields.date({
+          label: 'Updated date',
+          defaultValue: { kind: 'today' },
+        }),
+        draft: fields.checkbox({ label: 'Draft?', defaultValue: true }),
+        content: fields.markdoc({ label: 'Content' }),
+      },
+    }),
+  },
+})
---
+++ src/app/keystatic/layout.tsx
@@ -0,0 +1,6 @@
+import KeyStaticApp from './keystatic'
+
+export default function Layout() {
+  // @ts-expect-error
+  return <KeyStaticApp />
+}
