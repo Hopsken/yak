6fd495383cbc0319a2ee9c0c771f530489fcc22c
Hopsken <shaowei5@icloud.com>
2024-08-30 23:10:42 +0800

    refine font

--- src/components/Prose.tsx
+++ src/components/Prose.tsx
@@ -7,7 +7,7 @@ export function Prose({
   title?: string
 }>) {
   return (
-    <article className='prose dark:prose-invert prose-h1:text-xl prose-h1:font-bold prose-p:text-justify prose-a:text-blue-600 prose-img:rounded-xl'>
+    <article className='prose prose-slate text-balance lg:prose-lg prose-h1:text-xl prose-h1:font-bold prose-a:text-blue-600 prose-img:rounded-xl'>
       {title && <h1>{title}</h1>}
       {children}
     </article>
