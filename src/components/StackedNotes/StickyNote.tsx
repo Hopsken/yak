deae0b232f7ce2f15835883021010b30d328be46
Hopsken <shaowei5@icloud.com>
2024-08-31 11:59:18 +0800

    scroll into view

--- src/components/StackedNotes/StickyNote.tsx
+++ src/components/StackedNotes/StickyNote.tsx
@@ -1,10 +1,11 @@
 'use client'
 
 import { paneSettings } from '@/consts'
-import React, { useMemo } from 'react'
+import React, { useEffect, useLayoutEffect, useMemo, useRef } from 'react'
 import { Prose } from '../Prose'
 import clsx from 'clsx'
 import { useStackedStore } from './context'
+import scrollIntoView from 'scroll-into-view-if-needed'
 
 type Props = {
   index: number
@@ -13,13 +14,25 @@ type Props = {
 }
 
 export function StickyNote({ index, title, children }: Props) {
+  const ref = useRef<HTMLDivElement>(null)
   const offset = useMemo(() => calcOffset(index), [index])
   const { stacked, scrollTo } = useStackedStore()
   const willBeObscured = stacked > index + 0.8
   const withShadow = stacked > 0 && index <= stacked + 1
 
+  useLayoutEffect(() => {
+    if (ref.current) {
+      scrollIntoView(ref.current, {
+        scrollMode: 'if-needed',
+        behavior: 'smooth',
+        inline: 'center'
+      })
+    }
+  }, [])
+
   return (
     <div
+      ref={ref}
       className={clsx(
         'sticky top-0 w-full overflow-y-auto border-t bg-white first-of-type:border-none md:w-[625px] md:min-w-[625px] md:border-l md:border-t-0',
         { 'shadow-xl': withShadow }
