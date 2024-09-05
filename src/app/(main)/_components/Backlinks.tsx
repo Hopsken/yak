167ca6bb6121acf10c60c5d4f72c37346000ab0c
Hopsken <shaowei5@icloud.com>
2024-09-05 21:10:57 +0800

    hide backlinks section if none exit

--- src/app/(main)/_components/Backlinks.tsx
+++ src/app/(main)/_components/Backlinks.tsx
@@ -7,21 +7,25 @@ type Props = {
   backlinks: FullNote['backlinks']
 }
 
-export function Backlinks(props: Props) {
+export function Backlinks({ currentNote, backlinks }: Props) {
+  if (!backlinks.length) {
+    return null
+  }
+
   return (
     <section className='not-prose text-base font-medium'>
       <h3 className='text-base leading-loose text-gray-500'>
         Linked to this note
       </h3>
       <ul className='flex flex-col gap-2 leading-snug'>
-        {props.backlinks.map(({ slug, title }) => (
+        {backlinks.map(({ slug, title }) => (
           <li
             key={slug}
             className='inline-flex items-center gap-1 overflow-hidden text-ellipsis whitespace-nowrap'
           >
             <HiArrowDownLeft className='stroke-1' />
             <span className='underline'>
-              <HyperLink href={`/notes/${slug}`} from={props.currentNote}>
+              <HyperLink href={`/notes/${slug}`} from={currentNote}>
                 {title}
               </HyperLink>
             </span>
