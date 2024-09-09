b953f69041333cf477567d0b4acbde12cf8064b6
Hopsken <shaowei5@icloud.com>
2024-09-09 21:07:49 +0800

    minimal fields

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -28,16 +28,6 @@ export default config({
             description: "Don't change. Automatically generated from the title"
           }
         }),
-        pubDate: fields.date({
-          label: 'Publish date',
-          defaultValue: { kind: 'today' },
-          validation: { isRequired: true }
-        }),
-        updatedDate: fields.date({
-          label: 'Updated date',
-          defaultValue: { kind: 'today' }
-        }),
-        draft: fields.checkbox({ label: 'Draft?', defaultValue: true }),
         content: fields.markdoc({ label: 'Content' })
       }
     })
