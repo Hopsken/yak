357f137d74dccb2825b70d7501077fe638106c52
Hopsken <shaowei5@icloud.com>
2024-10-31 23:58:24 +0800

    update style

--- src/app/(main)/notes/page.tsx
+++ src/app/(main)/notes/page.tsx
@@ -1,6 +1,7 @@
 import { reader } from '@/lib/keystatic/reader'
 import { HiArrowDownLeft } from 'react-icons/hi2'
 import Link from 'next/link'
+import { Prose } from '@/components/Prose'
 
 export default async function NotesPage() {
   const notes = await reader.collections.notes.all()
@@ -8,22 +9,24 @@ export default async function NotesPage() {
   return (
     <main className='flex grow flex-col overflow-x-hidden md:flex-row md:overflow-x-auto md:overflow-y-hidden'>
       <div className='w-full md:flex md:grow'>
-        <section className='not-prose text-base font-medium'>
-          <h3 className='text-base leading-loose text-gray-500'>All notes</h3>
-          <ul className='flex flex-col gap-2 leading-snug'>
-            {notes.map(({ slug, entry }) => (
-              <li
-                key={slug}
-                className='inline-flex items-center gap-1 overflow-hidden text-ellipsis whitespace-nowrap'
-              >
-                <HiArrowDownLeft className='stroke-1' />
-                <span className='underline'>
-                  <Link href={`/notes/${slug}`}>{entry.title}</Link>
-                </span>
-              </li>
-            ))}
-          </ul>
-        </section>
+        <Prose>
+          <section className='not-prose p-8 text-base font-medium'>
+            <h3 className='text-base leading-loose text-gray-500'>All notes</h3>
+            <ul className='flex flex-col gap-2 leading-snug'>
+              {notes.map(({ slug, entry }) => (
+                <li
+                  key={slug}
+                  className='inline-flex items-center gap-1 overflow-hidden text-ellipsis whitespace-nowrap'
+                >
+                  <HiArrowDownLeft className='stroke-1' />
+                  <span className='underline'>
+                    <Link href={`/notes/${slug}`}>{entry.title}</Link>
+                  </span>
+                </li>
+              ))}
+            </ul>
+          </section>
+        </Prose>
       </div>
     </main>
   )
