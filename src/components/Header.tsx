2b20302f2dcbe628d4650bb7f381eeae3738361c
Hopsken <shaowei5@icloud.com>
2024-11-01 09:59:17 +0800

    fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,14 +5,15 @@ import Link from 'next/link'
 export function Header() {
   return (
     <header className='flex w-full flex-wrap items-center justify-between border-b px-8 py-3 dark:border-zinc-700'>
-      <div className='text-lg font-medium'>
+      <div className='flex items-center gap-4 text-lg font-medium'>
         <a href='/'>{siteSettings.title}</a>
+
+        <Link href='/notes' className='text-base text-zinc-600'>
+          {'~ls'}
+        </Link>
       </div>
 
-      <div className='flex items-center gap-4 text-base font-medium text-zinc-600'>
-        <Link href='/notes'>{'~ls'}</Link>
-        <ThemeToggle />
-      </div>
+      <ThemeToggle />
     </header>
   )
 }
